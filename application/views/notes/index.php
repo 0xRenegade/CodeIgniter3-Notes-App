384766613fb4f1790be3789674504783fb0d266e
0xRenegade <therenegadecoding@gmail.com>
2022-07-07 15:20:45 -0500

    add index page for all notes

---
+++ application/views/notes/index.php
@@ -0,0 +1,38 @@
+<div class="row">
+    <div class="text-center col-12">
+        <h1 class="text-center mt-5 mb-3">
+            Notes
+        </h1>
+    </div>
+</div>
+<div class="row">
+    <? foreach ($notes as $note): ?>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+            <div class="card">
+                <div class="card-header">
+                    <h3>
+                        <?=$note['title']?>
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <div class="card-text">
+                        <?=$notes['text']?>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-primary">
+                        <a href="<?=base_url('notes/show/'.$note->id)?>">View</a>
+                    </button>
+
+                    <button class="btn btn-warning">
+                        <a href="<?=base_url('notes/edit/'.$note->id)?>">Edit</a>
+                    </button>
+
+                    <button class="btn btn-danger">
+                        <a href="<?=base_url('notes/delete/'.$note->id)?>">Delete</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    <? endforeach; ?>
+</div>
