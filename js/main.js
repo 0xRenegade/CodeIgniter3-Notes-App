cead41eac196b076480fdf1649c51d88d10e8471
0xRenegade <therenegadecoding@gmail.com>
2022-07-07 18:34:07 -0500

    ajax + status message location added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('ready');
+$(document).ready(() => {
+    $('#create-note-form-button').on('click', (e) => {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: window.location.origin + '/notes/store',
+            dataType: 'json',
+            data: $('#create-note-form').serialize(),
+            success: (res) => {
+                $('.status').empty();
+                if (res.res == 'success') {
+                    $('.status').text(res.message);
+                    $('.status').css('color', 'green');
+                    $('.status').fadeIn(700);
+
+                    setTimeout(() => {
+                        window.location.replace(window.location.origin + '/notes/show/' + res.note_id);
+                    }, 1000);
+                } else if (res.res == 'error') {
+                    $('.status').text(res.message);
+                    $('.status').css('color', 'red');
+                    $('.status').fadeIn(700);
+                }
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        })
+    });
 });
