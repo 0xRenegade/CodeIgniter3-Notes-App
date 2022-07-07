91043f868e48f7a028e8ad80d790ccf89b9c8061
0xRenegade <therenegadecoding@gmail.com>
2022-07-07 14:42:26 -0500

    Notes_model

---
+++ application/models/Notes_model.php
@@ -0,0 +1,61 @@
+<?
+
+class Notes_model extends CI_Model {
+    public function __construct() {
+        $this->load->database();
+        $this->load->helper('url');
+    }
+
+    /*
+        Get all the notes from the database
+    */
+    public function get_all() {
+        $notes = $this->db->get("notes")->result();
+        return $notes;
+    }
+ 
+    /*
+        Store the note in the database
+    */
+    public function store() {    
+        $data = [
+            'title' => $this->input->post('title'),
+            'text'  => $this->input->post('text')
+        ];
+ 
+        $result = $this->db->insert('notes', $data);
+        return $result;
+    }
+ 
+    /*
+        Get an specific note from the database
+    */
+    public function get($id) {
+        $note = $this->db->get_where('notes', ['id' => $id ])->row();
+        return $note;
+    }
+ 
+ 
+    /*
+        Update or Modify a note in the database
+    */
+    public function update($id) {
+        $data = [
+            'title'        => $this->input->post('title'),
+            'text' => $this->input->post('text')
+        ];
+ 
+        $result = $this->db->where('id',$id)->update('notes',$data);
+        return $result;              
+    }
+ 
+    /*
+        Destroy or Remove a note in the database
+    */
+    public function delete($id) {
+        $result = $this->db->delete('notes', array('id' => $id));
+        return $result;
+    }
+}
+
+?>
