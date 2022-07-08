d90b1176eb70c995c092ba8cf57b1cadfdb1b049
0xRenegade <therenegadecoding@gmail.com>
2022-07-08 18:39:36 -0500

    add vendor via files instead of cdn

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,10 @@
     
         </div>
     </main>
-    <em>&copy; 2022</em>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="http://localhost:3000/js/main.js"></script>
+<script src="<?=base_url()?>js/vendor/bootstrap.min.js"></script> 
+<script src="<?=base_url()?>js/vendor/font-awesome.min.js"></script>
+<script src="<?=base_url()?>js/vendor/jquery.min.js"></script>
+<script src="<?=base_url()?>js/vendor/jquery-confirm.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>js/main.js"></script>
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,11 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="http://localhost:3000/css/style.css">
+    <link rel="stylesheet" href="<?=base_url()?>css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>css/vendor/font-awesome.min.css"/>
+    <link rel="stylesheet" href="<?=base_url()?>css/vendor/jquery-confirm.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>css/style.css">
     <title>CodeIgniter3 Notes App</title>
 </head>
 <body>
+    <div class="container-full">
+        <div class="container mb-3 mt-3">
+            <div class="row d-flex justify-content-start">
+                <div class="home">
+                    <button class="btn btn-primary">
+                        <a class="button-link" href="<?=base_url()?>">
+                            Home
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div> 
+
     <main id="app">
-        <div class="container">
+        <div class="container"> 
