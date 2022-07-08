db55464d9f930f810e5f3df1aa28b090ff43ad42
0xRenegade <therenegadecoding@gmail.com>
2022-07-08 12:11:38 -0500

    fix routes in buttons

--- application/views/notes/index.php
+++ application/views/notes/index.php
@@ -26,15 +26,15 @@
                 </div>
                 <div class="card-footer">
                     <button class="btn btn-primary">
-                        <a href="<?=base_url('notes/show/'.$note->id)?>">View</a>
+                        <a href="notes/show/<?=$note->id?>">View</a>
                     </button>
 
                     <button class="btn btn-warning">
-                        <a href="<?=base_url('notes/edit/'.$note->id)?>">Edit</a>
+                        <a href="notes/edit/<?=$note->id?>">Edit</a>
                     </button>
 
                     <button class="btn btn-danger">
-                        <a href="<?=base_url('notes/delete/'.$note->id)?>">Delete</a>
+                        <a href="notes/delete/<?=$note->id?>">Delete</a>
                     </button>
                 </div>
             </div>
